f74ed3e4e9ca53d68c28eca5557e1b437f086e96
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-03-08 15:11:18 +0100

    add form-validation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
-
+import Form from "./form.js";
+import Ui from "./ui.js";
 
 // Select elements
+const addForm = document.querySelector(".add-modal__form");
+const feedbackMessage = document.querySelector(".add-form__feedback-message");
+const openAddModalButton = document.querySelectorAll(".add-modal__button");
+const closeAddModalButton = document.querySelector(
+  ".add-modal__button--cancel"
+);
+const addModal = document.querySelector(".add-modal");
+
+openAddModalButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    Ui.openAddModal(closeAddModalButton, addModal, addForm);
+  });
+});
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  Form.formValidation(feedbackMessage);
+
+  if (!Form.formValidation(feedbackMessage)) {
+    console.log("form not submitted");
+    return;
+  }
+  feedbackMessage.textContent = "Submitted";
+});
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,1 +1,12 @@
-class Ui {}
+class Ui {
+  static renderStudent() {}
+  static openAddModal(closeAddModalButton, addModal, addForm) {
+    addModal.classList.add("add-modal--visible");
+    closeAddModalButton.addEventListener("click", () => {
+      addForm.reset();
+      addModal.classList.remove("add-modal--visible");
+    });
+  }
+}
+
+export default Ui;
