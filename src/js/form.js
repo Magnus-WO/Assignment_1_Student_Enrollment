1bd8e082054e0af9aedc1e7f54ccd0884480f7a5
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-03-09 19:58:29 +0100

    add form-validation and reset

---
+++ src/js/form.js
@@ -0,0 +1,51 @@
+class Form {
+  // Validation person input
+  static personFormValidation(feedbackMessage) {
+    feedbackMessage.textContent = "";
+    const fieldsToValidate = [
+      { name: "name", message: "Please enter your full name" },
+      { name: "email", message: "Please enter your email" },
+      { name: "courses", message: "Please select a course from the list" },
+    ];
+    for (let field of fieldsToValidate) {
+      const inputField = document.querySelector(`[id= ${field.name}]`);
+      inputField.addEventListener("input", () => {
+        feedbackMessage.textContent = "";
+        inputField.classList.remove("input-field__error");
+        feedbackMessage.classList.remove("input-field__error-message");
+      });
+      if (!inputField.value.trim()) {
+        feedbackMessage.textContent = field.message;
+        inputField.classList.add("input-field__error");
+        feedbackMessage.classList.add("input-field__error-message");
+        return false;
+      }
+    }
+    return true;
+  }
+  //   Validation course input
+  static courseFormValidation(feedbackMessage) {
+    feedbackMessage.textContent = "";
+    const fieldsToValidate = [
+      { name: "name", message: "Please enter your full name" },
+      { name: "course-code", message: "Please enter the course code" },
+    ];
+    for (let field of fieldsToValidate) {
+      const inputField = document.querySelector(`[id= ${field.name}]`);
+      inputField.addEventListener("input", () => {
+        feedbackMessage.textContent = "";
+        inputField.classList.remove("input-field__error");
+        feedbackMessage.classList.remove("input-field__error-message");
+      });
+      if (!inputField.value.trim()) {
+        feedbackMessage.textContent = field.message;
+        inputField.classList.add("input-field__error");
+        feedbackMessage.classList.add("input-field__error-message");
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+export default Form;
