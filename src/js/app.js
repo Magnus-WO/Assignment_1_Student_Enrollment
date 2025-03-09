3dd277962a2bf1be7ca47f24af93b7bfd796b8c0
Magnus Werner Ottesen <magnuswernerottesen@gmail.com>
2025-03-09 15:27:03 +0100

    added form-population based on which button is clicked

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,28 @@ import Ui from "./ui.js";
 
 // Select elements
 const addForm = document.querySelector(".add-modal__form");
+const formHeader = document.querySelector(".form__header");
+//    Form Containers
+const nameContainer = document.querySelector(".form__name-container");
+const emailContainer = document.querySelector(".form__email-container");
+const selectOptionsContainer = document.querySelector(
+  ".select-options-container"
+);
+const courseCodeContainer = document.querySelector(
+  ".form__course-code-container"
+);
+// Form inputs
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const selectDropDown = document.querySelector("#courses");
+const courseCodeInput = document.querySelector("#course-code");
+
+//Form labels
+const nameLabel = document.querySelector(".label__name");
+const emailLabel = document.querySelector(".label__email");
+const selectLabel = document.querySelector(".label__select");
+const courseCodeLabel = document.querySelector(".label__course-code");
+
 const feedbackMessage = document.querySelector(".add-form__feedback-message");
 const openAddModalButton = document.querySelectorAll(".add-modal__button");
 const closeAddModalButton = document.querySelector(
@@ -11,8 +33,15 @@ const closeAddModalButton = document.querySelector(
 const addModal = document.querySelector(".add-modal");
 
 openAddModalButton.forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (e) => {
     Ui.openAddModal(closeAddModalButton, addModal, addForm);
+    Ui.renderForm(
+      e,
+      formHeader,
+      emailContainer,
+      selectOptionsContainer,
+      courseCodeContainer
+    );
   });
 });
 
--- src/js/ui.js
+++ src/js/ui.js
@@ -7,6 +7,33 @@ class Ui {
       addModal.classList.remove("add-modal--visible");
     });
   }
+  static renderForm(
+    e,
+    formHeader,
+    emailContainer,
+    selectOptionsContainer,
+    courseCodeContainer
+  ) {
+    // Student
+    if (e.target.dataset.button === "student") {
+      formHeader.textContent = "Add student";
+      emailContainer.style.display = "flex";
+      selectOptionsContainer.style.display = "flex";
+      courseCodeContainer.style.display = "none";
+      // Instructor
+    } else if (e.target.dataset.button === "instructor") {
+      formHeader.textContent = "Add instructor";
+      emailContainer.style.display = "flex";
+      selectOptionsContainer.style.display = "flex";
+      courseCodeContainer.style.display = "none";
+      // Course
+    } else if (e.target.dataset.button === "course") {
+      formHeader.textContent = "Add course";
+      emailContainer.style.display = "none";
+      selectOptionsContainer.style.display = "none";
+      courseCodeContainer.style.display = "flex";
+    }
+  }
 }
 
 export default Ui;
