b64ee05e7dac95f97d3954883cff15f7977d3b00
Benedict Notoane <benni@MacBookPro.lan>
2025-03-09 17:54:27 +0100

    fixed syntax error in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,6 @@ const addStudentButton = document.querySelector(".add-student__button");
 const addInstructorButton = document.querySelector(".add-instructor__button");
 const addCourseButton = document.querySelector(".add-course__button");
 
-
 const addModal = document.querySelector(".add-modal");
 const formSubmitButton = document.querySelector(".add-modal__button--confirm");
 const closeModalButton = document.querySelector(".add-modal__button--cancel");
@@ -49,46 +48,45 @@ document.addEventListener("DOMContentLoaded", () => {
   Ui.closeAddModal(closeModalButton, addModal);
 });
 
-
 // Track student or instructor buttons
 let selectedPersonType = "";
 addStudentButton.addEventListener("click", () => {
   selectedPersonType = "student";
 
+  addInstructorButton.addEventListener("click", () => {
+    selectedPersonType = "instructor";
+  });
 
-addInstructorButton.addEventListener("click", () => {
-  selectedPersonType = "instructor";
-});
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    Form.formValidation(feedbackMessage);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  Form.formValidation(feedbackMessage);
+    // Form validation
+    if (!Form.formValidation(feedbackMessage)) {
+      console.log("form not submitted");
+      return;
+    }
+    // Not done ------------
+    //   Submit form
+    console.log("Name:", name.value.trim());
+    console.log("Email:", email.value.trim());
+    // Ensure you are getting the course value
+    console.log("Selected Course:", courseDropdown.value);
 
-  // Form validation
-  if (!Form.formValidation(feedbackMessage)) {
-    console.log("form not submitted");
-    return;
-  }
-// Not done ------------
-  //   Submit form
-  console.log("Name:", name.value.trim());
-  console.log("Email:", email.value.trim());
-  // Ensure you are getting the course value
-  console.log("Selected Course:", courseDropdown.value); 
+    if (!Ui.currentEditId) {
+      Manager.addPerson(
+        name.value.trim(),
+        email.value.trim(),
+        courseDropdown.value,
+        selectedPersonType
+      );
+      feedbackMessage.textContent = "Submitted";
+    } else {
+      // Manager.currentEditId()
+      Ui.currentEditId = null;
+      formSubmitButton.textContent = "Add";
+    }
 
-  if (!Ui.currentEditId) {
-    Manager.addPerson(
-      name.value.trim(),
-      email.value.trim(),
-      courseDropdown.value,
-      selectedPersonType
-    );
-    feedbackMessage.textContent = "Submitted";
-  } else {
-    // Manager.currentEditId()
-    Ui.currentEditId = null;
-    formSubmitButton.textContent = "Add";
-  }
-
-  form.reset();
+    form.reset();
+  });
 });
